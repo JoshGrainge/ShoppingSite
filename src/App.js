5f1ad6909cce50d8128067dbc70f2f346088290e
Josh Grainge <josh.grainge@hotmail.com>
2023-03-11 00:33:26 -0500

    Get all clothing items in Apps state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Bookmark from './components/Bookmark';
 import Header from './components/Header';
 import Home from './components/Home';
+import { getAllData } from './FakeStoreAPI';
 
 function App() {
+  const [shopItems, setShopItems] = useState();
+
+  useEffect(() => {
+    async function getDataOnLoad() {
+      const result = await getAllData();
+      setShopItems(result);
+    }
+    getDataOnLoad();
+  }, []);
+
+  console.log(shopItems);
+
   return (
     <div className="App">
       <Header />
       {/* <Home /> */}
-      <Bookmark />
+      {/* {shopItems && <Bookmark bookmarkItems={shopItems} />} */}
     </div>
   );
 }
