436ce171bc1b7b2c9726aff91357c534cc555547
Josh Grainge <josh.grainge@hotmail.com>
2023-03-11 19:43:08 -0500

    Created star rating component

---
+++ src/components/Rating.js
@@ -0,0 +1,19 @@
+function Rating({ rating, ratingCount }) {
+  const elements = [];
+  for (let i = 0; i < 5; i++) {
+    const starClass = rating > i ? 'fill star' : 'star';
+    const star = (
+      <span key={i} className={starClass}>
+        &#9733;
+      </span>
+    );
+    elements.push(star);
+  }
+
+  //   elements.push(<p style={{ color: 'black' }}>rating: {rating}</p>);
+  elements.push(<span>({ratingCount})</span>);
+
+  return <div className="star-container">{elements}</div>;
+}
+
+export default Rating;
