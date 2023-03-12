526e2fd3784d7a551e401e47b66e20db5ba538da
Josh Grainge <josh.grainge@hotmail.com>
2023-03-11 19:59:27 -0500

    Added products item page

---
+++ src/components/Products.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Products.css';
+import ProductCard from './ProductCard';
+
+function Products({ items }) {
+  return (
+    <main className="products-layout">
+      <div className="search-tab">
+        <div className="options-container">
+          <label>
+            Name:
+            <input />
+          </label>
+          <label>
+            Name:
+            <input />
+          </label>
+          <label>
+            Name:
+            <input />
+          </label>
+          <input type="range"></input>
+        </div>
+      </div>
+      <div className="products-container">
+        {items &&
+          items.map((item) => {
+            return (
+              <ProductCard
+                key={item.id}
+                name={item.title}
+                price={item.price}
+                image={item.image}
+                rating={item.rating.rate}
+                ratingCount={item.rating.count}
+              />
+            );
+          })}
+      </div>
+    </main>
+  );
+}
+
+export default Products;
