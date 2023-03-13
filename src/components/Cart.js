a771623c9880ff9c1758e5070db7f31427d5f885
Josh Grainge <josh.grainge@hotmail.com>
2023-03-13 19:47:47 -0400

    Improved cart stylings

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -46,14 +46,14 @@ function Cart({ cartItems, changeCartItemQuantity, removeItemFromCart }) {
                       onClick={() => changeCartItemQuantity(cartItem.item, -1)}
                       disabled={cartItem.quantity <= 1}
                     >
-                      -
+                      <i class="fa-solid fa-minus"></i>
                     </button>
                     <h2 className="quantity-text">{cartItem.quantity}</h2>
                     <button
                       className="circle-btn black bold big-font count-btn"
                       onClick={() => changeCartItemQuantity(cartItem.item, 1)}
                     >
-                      +
+                      <i class="fa-solid fa-plus"></i>
                     </button>
                   </div>
                   <h2 className="auto-margin">
