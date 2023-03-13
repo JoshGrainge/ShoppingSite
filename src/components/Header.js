8b5016f5cfae48096317c810aba469a11f03b44c
Josh Grainge <josh.grainge@hotmail.com>
2023-03-13 19:50:34 -0400

    Added routing ability & improved styling
    -Added dropdown menu for clothing categories
    -Added links for each category options
    -Added icons for bookmark and cart buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,62 @@
+import { Link, NavLink } from 'react-router-dom';
 import logo from '../logo.svg';
 import './Header.css';
 
 function Header() {
   return (
     <div id="header">
-      <div className="logo-section">
-        <img src={logo} alt="logo" id="logo" />
-        <h1 class="logo-text">
-          Kitty
-          <br />
-          City
-        </h1>
+      <>
+        <Link to="/" className="logo-section">
+          <img src={logo} alt="logo" id="logo" />
+          <h1 className="logo-text">
+            Kitty
+            <br />
+            City
+          </h1>
+        </Link>
+      </>
+      <div id="products">
+        <p>
+          Products <i class="fa-sharp fa-solid fa-caret-down"></i>
+        </p>
+        <nav className="product-links">
+          <NavLink className="dropdown-link" to="/products">
+            All
+          </NavLink>
+          <NavLink
+            className="dropdown-link"
+            to="/products?category=electronics"
+          >
+            Electronics
+          </NavLink>
+          <NavLink className="dropdown-link" to="/products?category=jewelery">
+            Jewelery
+          </NavLink>
+          <NavLink
+            className="dropdown-link"
+            to="/products?category=men's clothing"
+          >
+            Men's clothing
+          </NavLink>
+          <NavLink
+            className="dropdown-link"
+            to="/products?category=women's clothing"
+          >
+            Women's clothing
+          </NavLink>
+        </nav>
       </div>
-      <nav id="products">Products</nav>
       <div id="user-buttons">
-        <a href="/Bookmark">
-          <i>Bookmark</i>
-        </a>
-        <a href="/Cart">
-          <i>Cart</i>
-        </a>
+        <Link to="bookmark">
+          <button className="circle-btn black header-btn">
+            <i className="fa-solid fa-bookmark"></i>
+          </button>
+        </Link>
+        <Link to="cart">
+          <button className="circle-btn black header-btn">
+            <i class="fa-solid fa-cart-shopping"></i>
+          </button>
+        </Link>
       </div>
     </div>
   );
