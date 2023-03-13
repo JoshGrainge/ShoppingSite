acc84ea9d23597afb326f3a5d7c91e3276345d39
Josh Grainge <josh.grainge@hotmail.com>
2023-03-12 23:24:06 -0400

    Added ability to add items from cart and bookmark

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ import Cart from './components/Cart';
 import { getAllData } from './FakeStoreAPI';
 
 function App() {
+  // TODO make these an object instead of using multiple states
   const [shopItems, setShopItems] = useState();
   const [cartItems, setCartItems] = useState([]);
+  const [bookmarkItems, setBookmarkItems] = useState([]);
 
   useEffect(() => {
     async function getDataOnLoad() {
@@ -19,15 +21,90 @@ function App() {
     getDataOnLoad();
   }, []);
 
-  console.log(shopItems);
+  function addItemToCart(newItem) {
+    setCartItems((prev) => {
+      let hasItem = false;
+      // Add to quantity when item already in card
+      const mappedItems = prev.map((cartItem) => {
+        if (cartItem.item.id === newItem.id) {
+          hasItem = true;
+          return {
+            ...cartItem,
+            quantity: cartItem.quantity + 1,
+          };
+        }
+
+        return cartItem;
+      });
+
+      if (hasItem) {
+        return mappedItems;
+        // Add new item to cart
+      } else {
+        return [
+          ...prev,
+          {
+            item: newItem,
+            quantity: 1,
+          },
+        ];
+      }
+    });
+  }
+
+  function changeCartItemQuantity(item, quantityChange) {
+    setCartItems((prev) => {
+      return prev.map((cartItem) => {
+        if (cartItem.item.id === item.id) {
+          return {
+            ...cartItem,
+            quantity: cartItem.quantity + quantityChange,
+          };
+        }
+
+        return cartItem;
+      });
+    });
+  }
+
+  function removeItemFromCart(item) {
+    setCartItems((prev) =>
+      prev.filter((cartItem) => cartItem.item.id !== item.id)
+    );
+  }
+
+  function addItemToBookmark(item) {
+    // Only bookmark an item once
+    setBookmarkItems((prev) => {
+      return prev.indexOf(item) === -1 ? [...prev, item] : prev;
+    });
+  }
+
+  function removeFromBookmark(item) {
+    setBookmarkItems((prev) =>
+      prev.filter((bookmarkItem) => bookmarkItem !== item)
+    );
+  }
 
   return (
     <div className="App">
       <Header />
       {/* <Home /> */}
-      {/* {shopItems && <Bookmark bookmarkItems={shopItems} />} */}
-      {/* <Products items={shopItems} /> */}
-      <Cart cartItems={shopItems} />
+      {/* <Products
+        items={shopItems}
+        addToCart={addItemToCart}
+        addToBookmark={addItemToBookmark}
+      />
+      <Cart
+        cartItems={cartItems}
+        changeCartItemQuantity={changeCartItemQuantity}
+        removeItemFromCart={removeItemFromCart}
+      />
+      <Bookmark
+        bookmarkItems={bookmarkItems}
+        addToCart={addItemToCart}
+        removeFromBookmark={removeFromBookmark}
+      /> */}
     </div>
   );
 }
