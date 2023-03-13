9acb3d18e72ca1e8719544478212f46fcfbff520
Josh Grainge <josh.grainge@hotmail.com>
2023-03-13 19:51:29 -0400

    Get proper category for items or get all items if category is not passed

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Products.css';
 import ProductCard from './ProductCard';
 
-function Products({ items, addToCart, addToBookmark }) {
+function Products({ category, addToCart, addToBookmark }) {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    getItems();
+  }, [category]);
+
+  const getItems = async () => {
+    const url = category
+      ? `https://fakestoreapi.com/products/category/${category}`
+      : 'https://fakestoreapi.com/products';
+    const res = await fetch(url);
+    const data = await res.json();
+    setItems(data);
+  };
+
   return (
     <main className="products-layout">
       <div className="search-tab">
