5047c07ab7be1b43d91f8f29f8d4db94b7764ffc
Josh Grainge <josh.grainge@hotmail.com>
2023-03-14 03:28:11 -0400

    Added basic item filtering functionality

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,10 +4,51 @@ import ProductCard from './ProductCard';
 
 function Products({ category, addToCart, addToBookmark }) {
   const [items, setItems] = useState([]);
+  const [searchSettings, setSearchSettings] = useState({
+    minPrice: 0,
+    maxPrice: 0,
+    minStars: 1,
+  });
 
   useEffect(() => {
     getItems();
   }, [category]);
+
+  // Get min item value and max item value when new items are displayed
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    let min = items[0].price,
+      max = items[0].price;
+
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].price < min) min = items[i].price;
+      if (items[i].price > max) max = items[i].price;
+    }
+
+    setSearchSettings((prev) => {
+      return {
+        ...prev,
+        minPrice: min,
+        maxPrice: max,
+      };
+    });
+  }, [items]);
+
+  // Don't let min and max go out of bounds
+  useEffect(() => {
+    let newMinPrice = searchSettings.minPrice;
+    let newMaxPrice = searchSettings.maxPrice;
+
+    if (newMaxPrice < newMinPrice) newMaxPrice = newMinPrice;
+    if (newMinPrice > newMaxPrice) newMinPrice = newMaxPrice;
+
+    setSearchSettings((prev) => {
+      return { ...prev, minPrice: newMinPrice, maxPrice: newMaxPrice };
+    });
+  }, [searchSettings.minPrice, searchSettings.maxPrice]);
 
   const getItems = async () => {
     const url = category
@@ -18,28 +59,125 @@ function Products({ category, addToCart, addToBookmark }) {
     setItems(data);
   };
 
+  const filteredItems = items.filter((item) => {
+    return (
+      item.price >= searchSettings.minPrice &&
+      item.price <= searchSettings.maxPrice &&
+      Math.ceil(item.rating.rate) >= searchSettings.minStars
+    );
+  });
+
   return (
     <main className="products-layout">
       <div className="search-tab">
         <div className="options-container">
           <label>
-            Name:
-            <input />
+            Price Min
+            <input
+              type="number"
+              value={searchSettings.minPrice}
+              onChange={(e) => {
+                setSearchSettings((prev) => {
+                  return {
+                    ...prev,
+                    minPrice: Math.max(
+                      Math.min(e.target.value, prev.maxPrice),
+                      0
+                    ),
+                  };
+                });
+              }}
+            ></input>
           </label>
           <label>
-            Name:
-            <input />
+            Price Max
+            <input
+              type="number"
+              value={searchSettings.maxPrice}
+              onChange={(e) => {
+                setSearchSettings((prev) => {
+                  return {
+                    ...prev,
+                    maxPrice: Math.max(e.target.value, prev.minPrice),
+                  };
+                });
+              }}
+            ></input>
           </label>
-          <label>
-            Name:
-            <input />
-          </label>
-          <input type="range"></input>
+
+          <p>Min Star Rating:</p>
+          <div className="star-rating">
+            <label>
+              <input
+                type="radio"
+                name="star-rating-min"
+                value={1}
+                onChange={(e) => {
+                  setSearchSettings((prev) => {
+                    return { ...prev, minStars: e.target.value };
+                  });
+                }}
+              />
+              1
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="star-rating-min"
+                value={2}
+                onChange={(e) => {
+                  setSearchSettings((prev) => {
+                    return { ...prev, minStars: e.target.value };
+                  });
+                }}
+              />
+              2
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="star-rating-min"
+                value={3}
+                onChange={(e) => {
+                  setSearchSettings((prev) => {
+                    return { ...prev, minStars: e.target.value };
+                  });
+                }}
+              />
+              3
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="star-rating-min"
+                value={4}
+                onChange={(e) => {
+                  setSearchSettings((prev) => {
+                    return { ...prev, minStars: e.target.value };
+                  });
+                }}
+              />
+              4
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="star-rating-min"
+                value={5}
+                onChange={(e) => {
+                  setSearchSettings((prev) => {
+                    return { ...prev, minStars: e.target.value };
+                  });
+                }}
+              />
+              5
+            </label>
+          </div>
         </div>
       </div>
       <div className="products-container">
         {items &&
-          items.map((item) => {
+          filteredItems.map((item) => {
             return (
               <ProductCard
                 key={item.id}
